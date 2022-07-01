3b8acaa9f08e77940e8930a383ccbcff066d3801
koukemo <java.sparrow22.surface@gmail.com>
2022-07-01 14:51:30 +0900

    first commit

---
+++ custom_tutorials/src/custom_tutorials_talker.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# license removed for brevity
+import rospy
+from custom_tutorials_msgs.msg import CustomMessage
+
+def talker():
+    
+    pub = rospy.Publisher('chatter', CustomMessage, queue_size=10)
+    rospy.init_node('custom_tutorials_talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        data: CustomMessage
+        data.word = "hello world %s" % rospy.get_time()
+        data.number = 10
+        rospy.loginfo('data.word: %s' % data.word + '\n' 'data.number: %s' % str(data.number))
+        pub.publish(data)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
